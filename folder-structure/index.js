71f7be4c18be2fd808c0ddb70f7021986c3a56d5
berkaypacal <pacalberkay@gmail.com>
2022-09-09 23:57:44 +0300

    add createFolder functions

--- folder-structure/index.js
+++ folder-structure/index.js
@@ -1,4 +1,4 @@
-const fs = require("fs-extra");
+const fs = require('fs-extra');
 /**
  * TODO LIST
  * -Atomları moleküllerde, molekülleri organizmalarda çağırma opsiyonu
@@ -7,15 +7,15 @@ const fs = require("fs-extra");
  * config.json okuyup static variable at !TODO!
  *
  */
-var ATOMIC_FOLDERS = ["atoms", "molecules", "organisms", "templates"];
-fs.readJson("./test/folder-config.json")
+var ATOMIC_FOLDERS = ['atoms', 'molecules', 'organisms', 'templates'];
+fs.readJson('./test/folder-config.json')
   .then((folderConfig) => {
     //!TODO create folders
     ATOMIC_FOLDERS.map((atomicName) => {
-      createFolder(atomicName, basePath + "/components");
+      createFolder(atomicName, folderConfig.path + '/components');
       folderConfig.components[`${atomicName}`].map((folder) => {
-        createFolder(folder.name, basePath + "/components/" + componentFolder);
-        createFiles(folder, basePath + "/components/" + componentFolder + `/${folder.name}`);
+        createFolder(folder.name, folderConfig.path + '/components/' + atomicName);
+        createFiles(folder.name, folderConfig.path + '/components/' + atomicName + `/${folder.name}`,folderConfig.options);
         console.log(folder);
       });
     });
@@ -24,6 +24,15 @@ fs.readJson("./test/folder-config.json")
     console.error(err);
   });
 
-const createFolder = (folderName, path) => {};
+const createFolder = (folderName, path) => {
+  fs.ensureDir(`${path}/${folderName}`)
+  .then(() => {
 
-const createFiles = (fileName, path) => {};
+    //console.log('success!')
+  }).catch(err =>{
+    console.error(err);
+  })
+};
+
+
+
